5c173d914abf67a844fe08b24581c70d42195644
Adilson Futa <adilson.futa@jupiter.co.ao>
2023-04-17 12:21:48 +0100

    ajustes de fluxos

--- src/prismaORM/prisma.service.ts
+++ src/prismaORM/prisma.service.ts
@@ -5,10 +5,12 @@ import { PrismaClient } from "@prisma/client";
 @Injectable()
 export class prismaService extends PrismaClient implements OnModuleInit{
 
+    // iniciar conexao ao chamar o servico de PrismaService
    async onModuleInit() {
         await this.$connect();
     }
 
+    // Fechar Conexao ao Fechar o PrismaService ou app
     async EnableShutdownHooks( app: INestApplication ){
         this.$on('beforeExit', async ()=>{
             await app.close();
